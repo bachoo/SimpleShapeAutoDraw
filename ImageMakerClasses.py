26829a15d2c889c66765ada2282ef5633cacd437
Elco Bakker <3217796+bachoo@users.noreply.github.com>
2019-01-09 22:06:54 +0000

    seems to do what it's meant to do.

--- ImageMakerClasses.py
+++ ImageMakerClasses.py
@@ -16,33 +16,59 @@ class ImageMaker(object):
         if allowed_shapes is None:
             allowed_shapes = [RectMaker(),EllipseMaker()]
         self.shapes = allowed_shapes
-        self.rect_maker = RectMaker() # add this one separately to do backgrounds
+        # add this one separately to do backgrounds
+        self.bg_rect_maker = RectMakerCorners()
         # puts patterns n the form to be added as strings
         self.patterns = {pi:('url(#' + p.attrib['id'] + ')') for pi,p in enumerate(self.pattern_root.find('{'
                                                                       'http://www.w3.org/2000/svg}defs').getchildren())}
-    def make_1shape_image(self,save_location,include_mask=True):
+    def make_1shape_image(self, image_file_root, mask_file_root=None, bg_pattern=None, obj_pattern=None,
+                          obj_shape=None, obj_props=None):
         """Makes an image at the save location in which a patterned shape is added to a patterned location"""
-        new_tree = copy.deepcopy(self.pattern_tree)
-        root = new_tree.getroot()
-        layer1 = next(element for element in root.getchildren()
-                      if (element.tag == '{http://www.w3.org/2000/svg}g' and element.attrib['id'] == 'layer1'))
-        layer2 = next(element for element in root.getchildren()
-                      if (element.tag == '{http://www.w3.org/2000/svg}g' and element.attrib['id'] == 'layer2'))
-        self.rect_maker.add_shape(layer2, 100, 100, 200, 'white')  # add background
-        self.rect_maker.add_shape(layer2,100,100,200,self.patterns[1])
-        self.shapes[0].add_shape(layer1, 50, 50, 40, 'white') # add background
-        self.shapes[0].add_shape(layer1,50,50,40,self.patterns[3])
-        svg_loc = os.path.join(save_location,'image_1.svg')
-        png_loc = os.path.join(save_location, 'image_1.png')
-        new_tree.write(svg_loc)
-        # this exports the drawing, so if we want it to be a consistent square we have to keep everything inside
-        # the background rectangle. Experimented with exporting the background object specifically but the coordinates
-        # for export are not those given in creating the image. If desirable best to get the coordinates by picking
-        # out the background rectangle and requesting coordinates as:
-        #        x = os.system("inkscape -z %s -I object_id -X "%(svg_loc))
-        # etc.
+        if bg_pattern is None:
+            bg_pattern = self.random_pattern()
+        if obj_pattern is None:
+            obj_pattern = self.random_pattern()
+        if obj_shape is None:
+            obj_shape = self.random_obj_shape()
+        if obj_props is None:
+            obj_props = self.shapes[obj_shape].random_props()
 
-        os.system("inkscape -z %s -w 100 -h 100 -D -e %s"%(svg_loc,png_loc))
+        def make_2_shape_image_sub(bg_fill, obj_fill,file_name):
+            new_tree = copy.deepcopy(self.pattern_tree)
+            root = new_tree.getroot()
+            layer1 = next(element for element in root.getchildren()
+                          if (element.tag == '{http://www.w3.org/2000/svg}g' and element.attrib['id'] == 'layer1'))
+            layer2 = next(element for element in root.getchildren()
+                          if (element.tag == '{http://www.w3.org/2000/svg}g' and element.attrib['id'] == 'layer2'))
+            self.bg_rect_maker.add_shape(layer2, x0=0,y0=0,
+                                         width=config.bg_size[0],height=config.bg_size[1],
+                                         fill='white')  #add background
+            self.bg_rect_maker.add_shape(layer2, x0=0, y0=0,
+                                         width=config.bg_size[0], height=config.bg_size[1],
+                                         fill=bg_fill)
+            self.shapes[obj_shape].add_shape(layer1, fill= 'white', **obj_props) # add background
+            self.shapes[obj_shape].add_shape(layer1, fill= obj_fill, **obj_props)
+            svg_loc = file_name + '.svg'
+            png_loc = file_name + '.png'
+            new_tree.write(svg_loc)
+            # this exports the drawing, so if we want it to be a consistent square we have to keep everything inside
+            # the background rectangle. Experimented with exporting the background object specifically but the coordinates
+            # for export are not those given in creating the image. If desirable best to get the coordinates by picking
+            # out the background rectangle and requesting coordinates as:
+            #        x = os.system("inkscape -z %s -I object_id -X "%(svg_loc))
+            # etc.
+            os.system("inkscape -z %s -w %d -h %d -D -e %s"%(svg_loc,config.im_size[0],config.im_size[1],png_loc))
+        make_2_shape_image_sub(bg_pattern, obj_pattern, image_file_root)
+        if mask_file_root is not None:
+            make_2_shape_image_sub('black', 'white', mask_file_root)
+
+    def random_pattern(self):
+        """Returns a random pattern key from those available"""
+        return np.random.choice([x for x in self.patterns.values()])
+
+    def random_obj_shape(self):
+        """Returns a random pattern key from those available"""
+        return np.random.randint(len(self.shapes))
 
 
 class ShapeMaker(object):
@@ -55,6 +81,15 @@ class ShapeMaker(object):
 
     def add_shape(self, parent_element, cx, cy, size, fill, shape_id=None):
         pass
+
+    def random_props(self):
+        """get a set of random properties for an object (except for fill). Ranges taken from config."""
+        random_props = dict()
+        random_props['shape_id'] = np.random.randint(1000,9999)
+        random_props['cx'] = rand_range(None,config.obj_centre_lims[0],config.obj_centre_lims[2])
+        random_props['cy'] = rand_range(None, config.obj_centre_lims[1], config.obj_centre_lims[3])
+        random_props['size'] = rand_range(None,config.obj_size[0],config.obj_size[1])
+        return random_props
 
 
 class EllipseMaker(ShapeMaker):
@@ -75,6 +110,7 @@ class EllipseMaker(ShapeMaker):
                       'id':'path{:d}'.format(shape_id).zfill(4),
                       'style':self.style_string.format(str(fill))}
         circle = et.SubElement(parent_element, "ns0:ellipse", **shape_properties)
+
         return circle
 
 
@@ -100,7 +136,30 @@ class RectMaker(ShapeMaker):
         rect = et.SubElement(parent_element, "ns0:rect", **shape_properties)
         return rect
 
+class RectMakerCorners(object):
+    """like rect maker but you provide the corners x0,yo,x1,y1 instead"""
+    def __init__(self):
+        super().__init__()
+        self.style_string = 'opacity:1;fill:{:s};fill-opacity:1;stroke:none;stroke-width:3.11315393' \
+                            ';stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:4;' \
+                            'stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1'
+
+    def add_shape(self, parent_element, x0, y0,width,height, fill, shape_id=None):
+        if shape_id == None:
+            shape_id = np.random.randint(1000, 9999)
+
+        shape_properties = {'x': str(x0),
+                            'y': str(y0),
+                            'width': str(width),
+                            'height': str(height),
+                            'id': 'rect{:d}'.format(shape_id).zfill(4),
+                            'style': self.style_string.format(str(fill))}
+        rect = et.SubElement(parent_element, "ns0:rect", **shape_properties)
+
+def rand_range(shape,low=0,high=1):
+    """convenience function for getting a set of random numbers in a range"""
+    return np.random.random(shape)*(high-low) + low
 
 if __name__ == "__main__":
     image_maker_1 = ImageMaker()
-    image_maker_1.make_1shape_image('test_images')
+    image_maker_1.make_1shape_image(image_file_root='test_images/image_1',mask_file_root='test_images/mask_1')
---
+++ config.py
@@ -0,0 +1,4 @@
+bg_size = (200,200)
+im_size = (100,100)
+obj_size = (10,50)
+obj_centre_lims = (50,50,150,150) # x0:y0:x1:y1
